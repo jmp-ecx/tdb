166eb24ece96214235926640ae1abeb225acfd63
ReT-86 <remy.n.todd@gmail.com>
2024-05-20 12:58:21 -0700

    Get rid of expression stuff - too little time.

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from tdb.expr.repl import REPL
 import tdb.spec as spec
 import tdb
 
-# TODO - install script, which gets this all downloaded into the ProgramFiles directory
-
-# Testing configuration
-conf = spec.load('./config/.tdb/conf.json')
 
 def main() -> None:
-  proj = tdb.Project(conf)
-  for t in proj.tags:
-    t.print()
+  pass
 
 if __name__ == '__main__':
     main()
